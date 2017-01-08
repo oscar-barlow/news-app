d44579141541f5ff196c9efd74f55ed05277a780
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-08 21:30:01 +0000

    spike making the xhr request to guardian content

--- src/newsController.js
+++ src/newsController.js
@@ -4,8 +4,25 @@ function NewsController(articleList) {
 
 (function(exports) {
 
-  NewsController.prototype.articleList = function(){
+  NewsController.prototype.articleList = function( ){
     return this._articleList;
   };
+
+  NewsController.prototype.getNewsFromGuardian = function(url) {
+    this._xhr = new XMLHttpRequest();
+    request = this._xhr;
+
+    request.open("GET", "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/uk-news", true)
+    request.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        this.myText = JSON.parse(this.responseText);
+        console.log(this.myText);
+        console.log(this);
+      }
+    };
+    request.send();
+  };
+
+  exports.articleList = this.articleList;
 
 })(this);
