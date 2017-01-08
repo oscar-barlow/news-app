d935654427e1055056a01a83bd6c6a32bf991b4b
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-08 12:56:08 +0000

    article returns summary

--- spec/article-spec.js
+++ spec/article-spec.js
@@ -1,4 +1,9 @@
 (function testArticleCreation() {
-  var article = new Article();
+  var article = new Article("summary", "this is the full text lorem ipsum dolor sit amet");
   assert.isTrue(article);
+})();
+
+(function testArticleReturnsSummary(){
+  var article = new Article("summary", "this is the full text lorem ipsum dolor sit amet");
+  assert.isTrue(article.summary() === "summary")
 })();
--- src/article.js
+++ src/article.js
@@ -1,1 +1,13 @@
-function Article(){}
+function Article(summary, fullText) {
+  this._summary = summary;
+}
+
+(function(exports) {
+
+  Article.prototype.summary = function() {
+    return this._summary;
+  };
+
+  exports.summary = this.summary;
+
+})(this);
