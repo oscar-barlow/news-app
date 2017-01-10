baf7062c664df1b706d03baa600d3cd5c0e88eec
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-10 12:42:48 +0000

    controller can send xhr

--- spec/newsControllerSpec.js
+++ spec/newsControllerSpec.js
@@ -13,5 +13,27 @@
   var newsController = new NewsController(articleListDouble);
   newsController.makeXHR();
   assert.isTrue(newsController.unsentXHR().readyState === 0);
+})();
+
+(function testSendControllerXHR() {
+  function ArticleListDouble() {};
+  var articleListDouble = new ArticleListDouble();
+
+  function XHRDouble () {
+    this.sendCallCount = 0;
+  };
+
+  XHRDouble.prototype = {
+    send: function() {
+      this.sendCallCount++;
+    }
+  };
+
+  var xhrDouble = new XHRDouble();
+
+  var newsController = new NewsController(articleListDouble);
+  newsController._xhr = xhrDouble;
+  newsController.sendXHR()
+  assert.isTrue(xhrDouble.sendCallCount === 1);
 
 })();
